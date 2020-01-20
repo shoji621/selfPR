e47601b456711a0e30e6fbe74431ac3bc96bd102
shoji621 <takahiroshoji.ncu@gmail.com>
2020-01-20 17:39:40 +0900

    いいね非同期
    一覧表示画面での個別のビュー調整は後ほど

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,33 +1,27 @@
 class FavoritesController < ApplicationController
+  before_action :set_post
+
   def create
+    # binding.pry
     # こう記述することで、「current_userに関連したFavoriteクラスの新しいインスタンス」が作成可能。
     # つまり、favorite.user_id = current_user.idが済んだ状態で生成されている。
     # buildはnewと同じ意味で、アソシエーションしながらインスタンスをnewする時に形式的に使われる。
-    # respond_to do |format|
-    #   format.html {
-        favorite = current_user.favorites.build(post_id: params[:post_id])
-        favorite.save
-        redirect_to posts_path
-    #   }
-    #   format.json {
-    #     post = Post.find(params[:post_id])
-    #     render json: post.id
-    #   }
-    # end
+    favorite = current_user.favorites.build(post_id: params[:post_id])
+    favorite.save
   end
 
   def destroy
-    # respond_to do |format|
-    #   format.html { 
-        favorite = Favorite.find_by(post_id: params[:post_id], user_id: current_user.id)
-        favorite.destroy
-        redirect_to posts_path
-    #   }
-    #   format.json {
-    #     post = Post.find(params[:post_id])
-    #     render json: post.id
-    #   }
-    # end
+    # binding.pry
+
+    favorite = Favorite.find_by(post_id: params[:post_id], user_id: current_user.id)
+    favorite.destroy
   end
+
+  private
+  def set_post
+    @post = Post.find(params[:post_id])
+    @id_name = "favorite-#{@post.id}"
+  end
+
 end
 
